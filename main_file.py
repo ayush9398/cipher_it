138220ed03582910a6f33914db20fa93a1ac08d3
Ayush Mittal <ayush.mittal9398@gmail.com>
2018-03-29 14:15:48 +0530

    args_updation

--- main_file.py
+++ main_file.py
@@ -2,10 +2,13 @@ import argparse
 from Morse_code.morse import morse
 
 def main():
-    parser=argparse.ArgumentParser(description="Cipher your text")
+    parser=argparse.ArgumentParser(description="Cipher your text to the available cipher codes. Currently, available codes are:"
+                                               +"\n1. morse"
+                                   )
     parser.add_argument('-t',"--text", help="text you want to enter", type=str)
     parser.add_argument("cipher", help="enter the cipher to encode to")
     parser.add_argument("-f","--file", help="enter file to encode, .txt only")
+    parser.add_argument("-s","--save", help="Save to file")
     args=parser.parse_args()
     if (args.file):
         with open(args.file,'r') as filem:
@@ -16,7 +19,10 @@ def main():
         if (args.cipher == 'morse'):
             crypt = morse(args.text)
             print(crypt)
-
+    if(args.save):
+        with open(args.save,'a') as ofile:
+            ofile.write('\n'+crypt)
+            print("Saved to file "+ args.save)
 
 if __name__== "__main__":
     main()
